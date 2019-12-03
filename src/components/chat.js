486017208e23107701a07afd8442b3bfc8fea4b4
Dalton <eschberger.dalton@gmail.com>
2019-12-03 09:36:29 -0600

    stuck with transition to react

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 import "./chat.css";
 import "./script"
+import io from "socket.io"
 export default class Chat extends Component {
+    constructor(){
+        super();
+        const socket = io('http://localhost:3000');
+        const messageForm = document.getElementById("send-container");
+        const messageInput = document.getElementById("message-input");
+        const name = prompt('What is your name?');
+    }
+    appendMessage(message){
+        const messageContainer = document.getElementById("message-container");
+        const messageElement = document.createElement('div');
+        messageElement.innerText=message;
+        messageContainer.append(messageElement);
+    }
+
     render() {
         return (
             <div>
